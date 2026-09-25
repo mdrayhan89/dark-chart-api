4c7b1e150483cae411e81c1d2332e431c9413109
mdrayhan89 <islamrabby655@gmail.com>
2026-09-25 23:13:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# Templates directory link
-templates = Jinja2Templates(directory="templates")
+# Absolute path set to locate templates directory accurately on Render
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 SUPPORTED_PAIRS = [
     "EUR/USD", "USD/JPY", "CAD/JPY", "AUD/CAD", 
